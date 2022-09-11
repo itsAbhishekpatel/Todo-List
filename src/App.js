14b34c363b2121c6b616a5a2bed36b96441c67f7
abhishek <itsabhishek137@gmail.com>
2022-09-11 17:16:51 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Compenents/Header';
+import { Todos } from './Compenents/Todos';
+import { TodosItem } from './Compenents/TodosItem';
+import { Footer } from './Compenents/Footer';
+import React,{useState} from 'react';
 
 function App() {
+  const onDelete =  (todo) =>{
+    console.log("I am onDelete",todo)
+
+    setTodos(todos.filter((e)=>{
+      return e!==todo ;
+
+    }));
+
+
+  }
+
+  const [todos, setTodos] = useState( [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to market to buy vegs."
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the mall to do shopping."
+    },
+    {
+      sno: 3,
+      title: "Go for interview",
+      desc: "Be prepare for the interview."
+    },
+  ]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title = "My Todos List" />
+      <Todos todos = {todos} onDelete = { onDelete} />
+      {/* <Footer /> */}
+
     </div>
   );
 }
---
+++ src/Compenents/Todos.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TodosItem } from './TodosItem'
+
+export const Todos = (props) => {
+  return (
+    <div className='Container'>
+      <h3 className='text-center my-3'>
+        Todos List
+      </h3>
+      {props.todos.map((todo)=>{
+        return <TodosItem todo = {todo} key = {todo.sno} onDelete = {props.onDelete} />
+
+      })}
+      
+
+
+    </div>
+  )
+}
